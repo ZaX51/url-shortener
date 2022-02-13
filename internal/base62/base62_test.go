e5e41bba7541717437ad51442f7848a20452f42f
Dawid Sołtysiak <d.soltysiak51@gmail.com>
2022-02-13 15:04:46 +0100

    Improve tests

--- internal/base62/base62_test.go
+++ internal/base62/base62_test.go
@@ -17,23 +17,29 @@ func TestTobase64EmptyArray(t *testing.T) {
 func TestTobase64OneItemArray(t *testing.T) {
 	result := base62.ToBase62([]byte{0x61})
 
-	if result != "1Z" {
-		t.Errorf("ToBase62 = %s; want '1Z' string", result)
+	expected := "1Z"
+
+	if result != expected {
+		t.Errorf("ToBase62 = %s; want '%s' string", result, expected)
 	}
 }
 
 func TestTobase64String(t *testing.T) {
 	result := base62.ToBase62([]byte("qwertyzaerty"))
 
-	if result != "jfXgVTpHPdtEqTpB" {
-		t.Errorf("ToBase62 = %s; want 'jfXgVTpHPdtEqTpB' string", result)
+	expected := "jfXgVTpHPdtEqTpB"
+
+	if result != expected {
+		t.Errorf("ToBase62 = %s; want '%s' string", result, expected)
 	}
 }
 
 func TestTobase64UrlString(t *testing.T) {
 	result := base62.ToBase62([]byte("https://test.test"))
 
-	if result != "D7qW2GM8nm2wH4FoGdNnDXw" {
-		t.Errorf("ToBase62 = %s; want 'D7qW2GM8nm2wH4FoGdNnDXw' string", result)
+	expected := "D7qW2GM8nm2wH4FoGdNnDXw"
+
+	if result != expected {
+		t.Errorf("ToBase62 = %s; want '%s' string", result, expected)
 	}
 }
